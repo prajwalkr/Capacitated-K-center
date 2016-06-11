9deeeb8545efff126b159a2aa24337a80256ecf6
prajwal <prajwalrenukanand@gmail.com>
2016-06-11 21:21:33 +0530

    Refactored code and added objective function

--- lp.py
+++ lp.py
@@ -2,39 +2,36 @@ import snap
 from pulp import *
 from pprint import pprint
 
-UGraph = snap.GenRndGnm(snap.PUNGraph, 10, 30)
-w,h=10,10
-Adj = [[0 for x in range(w)] for y in range(h)]
-for EI in UGraph.Edges():
-	Adj[EI.GetSrcNId()][EI.GetDstNId()]=1
-	Adj[EI.GetDstNId()][EI.GetSrcNId()]=1
-for x in range(h):
-	for y in range(w):
-		print Adj[x][y],
-	print
+def randomGraph(n,edges):
+	UGraph = snap.GenRndGnm(snap.PUNGraph, n, edges)
+	adj = [[0 for x in range(n)] for y in range(n)]
+	for EI in UGraph.Edges():
+		adj[EI.GetSrcNId()][EI.GetDstNId()] = 1
+		adj[EI.GetDstNId()][EI.GetSrcNId()] = 1
+	'''for x in range(h):
+					for y in range(w):
+						print adj[x][y],
+					print'''
 
+N, M = 10, 30
+adj = randomGraph(N, M)
 L,k=3,4
 
 # declare your variables
-cen = dict(zip([i for i in range(h)],[1 for i in range(h)]))		
-cen_dict=LpVariable.dicts("cen_dicts",cen, 0, 1,LpInteger)  
-res={}
-for i in range(h):
-	for j in range(w):
-		res[(i,j)]=Adj[i][j]
-res_dict=LpVariable.dicts("res_dicts",res,0,1)
+cen = dict(zip([i for i in range(N)],[1 for i in range(N)]))		
+cen_dict = LpVariable.dicts("cen",cen, 0, 1,LpInteger)
 
+res = {}
+for i in range(N):
+	for j in range(N):
+		res[(i,j)] = adj[i][j]
+res_dict = LpVariable.dicts("res",res,0,1)
 
 # defines the problem
 prob = LpProblem("problem", LpMinimize)
 
-
-# defines the constraints
-prob += sum(cen) <= k
-
-
 # defines the objective function to minimize
-prob += lpSum(cen)
+prob += lpSum([cen_dict[i] for i in range(10)])
 
 # solve the problem
 status = prob.solve(GLPK(msg=0))
