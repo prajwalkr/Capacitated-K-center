974829e87372ec5f58e4ea6552cc1e4b323cd33c
prajwal <prajwalrenukanand@gmail.com>
2016-06-13 19:02:22 +0530

    Got opt working, fixed all bugs

--- opt.py
+++ opt.py
@@ -1,19 +1,24 @@
 import snap
 from pulp import *
-from pprint import pprint
 
 def randomGraph(n,edges):
 	UGraph = snap.GenRndGnm(snap.PUNGraph, n, edges)
-	adj = [[0 for x in range(n)] for y in range(n)]
+	adj = dict()
+	for x in range(n):
+		for y in range(n):
+			adj[(x,y)] = 0
 	for EI in UGraph.Edges():
-		adj[EI.GetSrcNId()][EI.GetDstNId()] = 1
-		adj[EI.GetDstNId()][EI.GetSrcNId()] = 1
-	'''for x in range(h):
-					for y in range(w):
-						print adj[x][y],
-					print'''
+		adj[(EI.GetSrcNId(),EI.GetDstNId())] = 1
+		adj[(EI.GetDstNId(), EI.GetSrcNId())] = 1
+	for i in xrange(n):
+		adj[(i,i)] = 1
+	for x in range(n):
+		for y in range(n):
+			print adj[(x,y)],
+		print
+	return adj
 
-N, M = 10, 30
+N, M = 10, 10
 adj = randomGraph(N, M)
 L,k = 3,4
 
@@ -24,7 +29,8 @@ cen = LpVariable.dicts("cen",cen, 0, 1,LpInteger)
 assignments = {}
 for i in range(N):
 	for j in range(N):
-		assignments[(i,j)] = i == j
+		assignments[(i,j)] = adj[(i,j)]
+
 assignments = LpVariable.dicts("assignments",assignments,0,1)
 
 # defines the problem
@@ -48,15 +54,12 @@ for i in xrange(N):
 	for j in xrange(N):
 		prob += assignments[(i,j)] <= cen[i]
 
-# Non - negativity constraints
-for i in xrange(N):
-	prob += cen[i] >= 0
 for i in xrange(N):
 	for j in xrange(N):
-		prob += assignments[(i,j)] >= 0
+		prob += assignments[(i,j)] <= adj[(i,j)]
 
 # solve the problem
-status = prob.solve()
+status = prob.solve(GLPK(msg=0))
 print LpStatus[prob.status]
 for v in prob.variables():
     print(v.name, "=", v.varValue)
